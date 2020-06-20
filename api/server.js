6edfd359bf078a82eb69d01a235fe0075203d0fc
adastraz <tylergosox@gmail.com>
2020-06-20 19:08:47 -0400

    initial routers

--- api/server.js
+++ api/server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const authRouter = require('./auth/authRouter')
+const postsRouter = require('./posts/postsRouter')
 
 const server = express()
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
 
+server.use('/api', authRouter)
+server.use('api/posts', postsRouter)
+
 server.get('/', (req, res) => {
-    res.json({ message: 'Server is up and running'})
+    res.json({ message: 'Server is up and running' })
 })
 
 module.exports = server
