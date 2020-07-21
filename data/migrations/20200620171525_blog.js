3a3948c31ffcf984b174a284e4eb917032458632
adastraz <tylergosox@gmail.com>
2020-07-21 18:23:40 -0400

    updated the migrations to allow for username

--- data/migrations/20200620171525_blog.js
+++ data/migrations/20200620171525_blog.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
     return knex.schema
         .createTable('users', tbl => {
             tbl.increments()
+            users.string('username', 255).notNullable().unique()
             tbl.string('password').notNullable().unique()
         })
 
